44b7175716cc13098f19f95a4ddcb3bb5f700323
Camilo Vargas <camilovargas2112@gmail.com>
2025-11-16 15:03:01 -0500

    arreglo para detectar las variables de entorno rapidamente para supabase

--- src/supabase.client.ts
+++ src/supabase.client.ts
@@ -1,6 +1,17 @@
-import { createClient } from '@supabase/supabase-js'
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const url = process.env.SUPABASE_URL || ''
-const key = process.env.SUPABASE_KEY || ''
+import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(url, key)
+const url = process.env.SUPABASE_URL;
+const key = process.env.SUPABASE_KEY;
+
+if (!url) {
+  console.error('[❌ Supabase Error] SUPABASE_URL is undefined');
+}
+
+if (!key) {
+  console.error('[❌ Supabase Error] SUPABASE_KEY is undefined');
+}
+
+export const supabase = createClient(url as string, key as string);
