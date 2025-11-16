6dd0c39ef5c35f3bd15c57ddab9c5f308874ab2a
Camilo Vargas <camilovargas2112@gmail.com>
2025-11-16 14:22:57 -0500

    Correciones para que funcione el enviar correo en railway

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -28,7 +26,8 @@ async function bootstrap() {
   });
 
   const PORT = process.env.PORT || 3000;
-  await app.listen(PORT);
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  await app.listen(PORT, '0.0.0.0');
+
+  console.log(`🚀 Server is running on port ${PORT}`);
 }
 bootstrap();
